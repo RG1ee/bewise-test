44941ccb43fb8bde1393897800ff151f83410e54
RG1ee <niwows@yandex.ru>
2023-10-11 21:17:59 +0300

    setup sqlalchemy and alembic, create table for question and answer, create config with app settings

---
+++ src/database/models/question.py
@@ -0,0 +1,13 @@
+import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+
+
+class Question(Base):
+    __tablename__ = "question"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
+    question: Mapped[str]
+    answer: Mapped[str]
+    created_at: Mapped[datetime.datetime]
